16b5ce8e9d2c1ecf2db95cc1b0350b705f2dd4b9
rishav09 <rishavsapahia@gmail.com>
2020-09-29 17:37:13 -0400

    Updated Path

--- image_resize.py
+++ image_resize.py
@@ -16,7 +16,7 @@ def resize_image(image_path,output_folder,resize):
     )
     img.save(outpath)
 
-path_dir = '/dkube/users/rishav09/dataset/Aeon/'
+path_dir = '/dkube/users/rishav09/dataset/Aeon/Mod_AEON_Data'
 output_folder = '/dkube/users/rishav09/dataset/NewResizedDataset'
 images = glob.glob(os.path.join(path_dir,'*.JPG'))
 
